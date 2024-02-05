9ab111300f348438f7c8dd53160f48584236022b
valerioperria <valerio.perria@gmail.com>
2024-02-05 13:56:03 +0100

    fix. ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,25 +8,30 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $projects = Project::all();
-        return response()->json(['result' => $projects]);
-    }
-
-    public function show(string $slug) {
-        $project = Project::with('technologies')->where('slug', $slug)->first();
-
-        if($project) {
-
         return response()->json([
-            'results' => $project,
+            'result' => $projects,
             'success' => true
         ]);
-    } else {
-        return response()->json([
-            'success' => false,
-            'message' => 'No project available'
-        ]);
     }
+
+    public function show(string $slug)
+    {
+        $project = Project::with('technologies')->where('slug', $slug)->first();
+
+        if ($project) {
+
+            return response()->json([
+                'results' => $project,
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'No project available'
+            ]);
+        }
     }
 }
